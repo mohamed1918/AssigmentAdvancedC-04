7ee2865be97c635114bd8554297f4314075e4082
mohamed1918 <memoohossam2004@gmail.com>
2025-08-06 21:48:20 +0300

    Q2

--- AssigmentAdvancedC#04/Program.cs
+++ AssigmentAdvancedC#04/Program.cs
@@ -7,18 +7,33 @@ namespace AssigmentAdvancedC_04
         static void Main(string[] args)
         {
             #region Q1
-            int[] numbers = { 1, 2, 2, 3, 4, 3, 1, 1 };
-            Hashtable freqTable = new Hashtable();
-            foreach (int num in numbers)
+            //int[] numbers = { 1, 2, 2, 3, 4, 3, 1, 1 };
+            //Hashtable freqTable = new Hashtable();
+            //foreach (int num in numbers)
+            //{
+            //    if (freqTable.ContainsKey(num))
+            //        freqTable[num] = (int)freqTable[num] + 1;
+            //    else
+            //        freqTable[num] = 1;
+            //}
+            //Console.WriteLine("Frequencies:");
+            //foreach (DictionaryEntry entry in freqTable)
+            //    Console.WriteLine($"{entry.Key}: {entry.Value}");
+            #endregion
+
+            #region Q2
+            Hashtable vaules = new Hashtable { {"mohammed",21 }, { "mohaned", 18 } , { "mariam", 13 } };
+            string maxKey = "";
+            int maxValue = int.MinValue;
+            foreach (DictionaryEntry entry in vaules)
             {
-                if (freqTable.ContainsKey(num))
-                    freqTable[num] = (int)freqTable[num] + 1;
-                else
-                    freqTable[num] = 1;
+                if ((int)entry.Value > maxValue)
+                {
+                    maxValue = (int)entry.Value;
+                    maxKey = (string)entry.Key;
+                }
             }
-            Console.WriteLine("Frequencies:");
-            foreach (DictionaryEntry entry in freqTable)
-                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            Console.WriteLine($"Key with max value: {maxKey} with value: {maxValue}");
             #endregion
         }
     }
