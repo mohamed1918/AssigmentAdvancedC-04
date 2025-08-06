25527a16e16fc068894c4b9586aa0fce08b71ba9
mohamed1918 <memoohossam2004@gmail.com>
2025-08-06 21:37:49 +0300

    Q1

--- AssigmentAdvancedC#04/Program.cs
+++ AssigmentAdvancedC#04/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace AssigmentAdvancedC_04
+﻿using System.Collections;
+
+namespace AssigmentAdvancedC_04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            int[] numbers = { 1, 2, 2, 3, 4, 3, 1, 1 };
+            Hashtable freqTable = new Hashtable();
+            foreach (int num in numbers)
+            {
+                if (freqTable.ContainsKey(num))
+                    freqTable[num] = (int)freqTable[num] + 1;
+                else
+                    freqTable[num] = 1;
+            }
+            Console.WriteLine("Frequencies:");
+            foreach (DictionaryEntry entry in freqTable)
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            #endregion
         }
     }
 }
